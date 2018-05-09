b6cd35713bd08bac7ef6b60b79d22ef3ffa5a58a
xiongjian <webjsforyou@gmail.com>
2018-05-09 16:08:23 +0800

    搭建首页和爬取豆瓣内容

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const koa = require('koa2')
+const render = require('koa-ejs');
+const path = require('path');
+// const {commonTpl} = require('./tpl')
+
 const app = new koa()
 
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: false,  //默认布局的文件
+  viewExt: 'html', //文件后缀名
+  cache: false, //是否缓存
+  debug: false //是否调试bug
+})
+
 app.use(async (ctx, next) => {
-  ctx.body = '电影首页'
+  const users = [{
+    name: 'sunwukong',
+    age: 18
+  },{
+    name: 'zhubajie',
+    age: 16
+  }]
+
+  await ctx.render('template', {users})
 })
 
 app.listen(3000, () => console.log('服务器启动成功了'))
---
+++ server/crawler/index.js
@@ -0,0 +1,58 @@
+const puppeteer = require('puppeteer')
+
+const url = `https://movie.douban.com/tag/#/?sort=R&range=6,10&tags=%E7%94%B5%E5%BD%B1`
+
+function sleep (time) {
+  return new Promise(resolve => {
+    setTimeout(resolve, time)
+  })
+}
+
+;(async () => {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox'],
+    dumpio: false
+  })
+  const page = await browser.newPage()
+  await page.goto(url, {
+    waitUntil: 'networkidle2'
+  })
+
+  await sleep(3000)
+
+  await page.waitForSelector('.more')
+
+  for (let i = 0; i < 1; i++) {
+    await sleep(3000)
+    await page.click('.more')
+  }
+
+  const result = await page.evaluate(() => {
+    var $ = window.$
+    var items = $('.list-wp>a')
+    var links = []
+
+    if (items.length >= 1) {
+      items.each((index, item) => {
+        let it = $(item)
+        let doubanId = it.find('div').data('id')
+        let title = it.find('.title').text()
+        let rate = +it.find('.rate').text()
+        let poster = it.find('img').attr('src').replace('s_ratio', 'l_ratio')
+
+        links.push({
+          doubanId,
+          title,
+          rate,
+          poster
+        })
+      })
+    }
+
+    return links
+  })
+
+  await browser.close();
+
+  console.log(result)
+})()
